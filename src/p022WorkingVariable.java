7df8b66783cd2a0d1d33b901c51c7b9e48e6dfbe
Adha Rizki Khawa Rizmi <adharizkisujaini@gmail.com>
2026-02-12 16:53:47 +0700

    first commit

---
+++ src/p022WorkingVariable.java
@@ -0,0 +1,16 @@
+public class p022WorkingVariable {
+    public static void main(String[] args) {
+        
+        // Deklarasi dan insialisasi variabel
+        int angka = 10;
+
+        // Menampilkan nilai awal variabel
+        System.out.println("Nilai awal angka: " +angka);
+
+        // Mengubah nilai varibel
+        angka = 25;
+
+        // Menampilkan nilai setelah perubahan
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+}
